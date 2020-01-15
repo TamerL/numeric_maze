cacb73428761da9dcd264f7d60d39e6067b5c98b
tamerl <eng.tamereid@gmail.com>
2020-01-15 20:08:05 +1100

    Removing unneeded logs of the function code

--- src/solve.rb
+++ src/solve.rb
@@ -1,35 +1,23 @@
 def solve(a,b)
   ways = [[a]]
   result = [a]
-  sol = []
   while result[-1] != b do
     ways.each_with_index do |arr,ind|
       ways.each do |del|
         ways.delete_at(ways.index(del)) if del.length > ind + 1
       end
-      pp "ways #{ways}"
-      pp "arr #{arr}"
       result = arr.dup
-      # if arr.include?(1)
-      #   ways.delete_at(ways.index(arr))
-      #   next
-      # end
       insert = [arr[-1] * 2 , 2 + arr[-1]]
       insert << arr[-1] / 2 if arr[-1] % 2 == 0
       insert.uniq!
       insert.each do |val|
-        # pp "result #{result}"
         result = arr.dup
         result << val
         ways << result
-        pp "result #{result}"
       end
-      pp "ways #{ways}"
-      # pp "result #{result}"
-      # ways.delete_at(ways.index(arr))
       ways.sort_by(&:length)
       break if result[-1] == b
     end
   end
-  pp result
+  result
 end

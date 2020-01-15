e71e0965f7c5cd7b2f9d245f5697ecc885d6b508
tamerl <eng.tamereid@gmail.com>
2020-01-15 23:27:04 +1100

    Another implementation, refactoring the code

--- src/solve.rb
+++ src/solve.rb
@@ -1,23 +1,27 @@
 def solve(a,b)
-  ways = [[a]]
-  result = [a]
-  while result[-1] != b do
-    ways.each_with_index do |arr,ind|
-      ways.each do |del|
-        ways.delete_at(ways.index(del)) if del.length > ind + 1
-      end
-      result = arr.dup
-      insert = [arr[-1] * 2 , 2 + arr[-1]]
-      insert << arr[-1] / 2 if arr[-1] % 2 == 0
-      insert.uniq!
-      insert.each do |val|
-        result = arr.dup
-        result << val
-        ways << result
-      end
-      ways.sort_by(&:length)
-      break if result[-1] == b
+arr1 = [[a]]
+arr2 = []
+while true do
+  arr2 = []
+  while arr1.length > 0 do
+
+    temp = arr1.shift
+    element = temp[-1]
+
+    return temp + [b] if element * 2 == b || element + 2 == b
+
+    insert = [element * 2, element + 2]
+
+    insert << element / 2 if (element % 2 == 0 && element != 2)
+
+    return temp + [b] if insert.include?(b)
+
+    insert.uniq.each do |dig|
+      result = temp.dup
+      arr2 << result + [dig]
     end
   end
-  result
+
+  arr1 = arr2.dup
+end
 end
